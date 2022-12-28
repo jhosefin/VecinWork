103fff71b8246836b542c88bc69c8ff8f6a533dd
jhosefin <90260920+jhosefin@users.noreply.github.com>
2022-12-28 15:59:41 -0300

    subiendo

--- Vecinwork React/vecinwork/src/App.js
+++ Vecinwork React/vecinwork/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Homepage, Conocenospage, ChatUsuariopage, CategoriasDestacadasPage, PublicacionPage, Usuario, InicioSesionPage,PerfilPage} from './pages';
+import { IngresoUsuario } from './components';
+import Publicar from './components/Publicar';
+import PublicarPage from './pages/PublicarPage';
 
 const App = () =>{
     return(
@@ -17,6 +20,13 @@ const App = () =>{
         <Route path='/Registro' element={<Usuario/>}/>
         <Route path='/Iniciar-Sesion' element={<InicioSesionPage/>}/>
         <Route path='/Perfil' element={<PerfilPage/>}/>
+        <Route path='/IngresoUsuario' element={<IngresoUsuario/>}/>
+
+
+<Route path='/Publicacion' element={<PublicacionPage/>} />
+<Route path='/Publicar' element={<PublicarPage/>}/>
+
+
       </Routes>
       </BrowserRouter>
     )
